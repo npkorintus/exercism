2bfab23f8b701f042e054651e662aba51335222f
Nick Korintus <nicholas.korintus@gmail.com>
2024-03-07 13:07:58 -0500

    javascript/lasagna-master: implemented cookingStatus, preparationTime functions

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -1,6 +1,7 @@
 /// <reference path="./global.d.ts" />
 // @ts-check
 
+
 /**
  * Implement the functions needed to solve the exercise here.
  * Do not forget to export them so they are available for the
@@ -10,3 +11,31 @@
  *   ...
  * }
  */
+
+/**
+ * Determine whether the lasagna is done
+ *
+ * @param {number} remainingTime
+ *
+ * @returns {string} status of lasagna based on remaining time on timer
+ */
+export function cookingStatus(remainingTime) {
+  if (remainingTime === 0)
+    return 'Lasagna is done.';
+  else if (remainingTime > 0)
+    return 'Not done, please wait.';
+  else
+    return 'You forgot to set the timer.'
+}
+
+/**
+ * Estimate the preparation time
+ *
+ * @param {string[]} layers
+ * @param {number} avgPrepTime
+ *
+ * @returns {number} estimated total prep time
+ */
+export function preparationTime(layers, avgPrepTime = 2) {
+  return layers.length * avgPrepTime;
+}
