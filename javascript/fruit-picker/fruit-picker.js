0efea0d7057c638e79703fde296f1ce644d304f4
Nick Korintus <nicholas.korintus@gmail.com>
2024-03-12 21:32:58 -0400

    javascript/fruit-picker: implemented orderFromGrocer, postOrder functions

--- javascript/fruit-picker/fruit-picker.js
+++ javascript/fruit-picker/fruit-picker.js
@@ -33,7 +33,7 @@ export function onError() {
  * @return void
  */
 export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
-  // implement the orderFromGrocer function to order the query
+  return order(query, onSuccessCallback, onErrorCallback);
 }
 
 /**
@@ -42,5 +42,5 @@ export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
  * @return void
  */
 export function postOrder(variety, quantity) {
-  //implement the postOrder function to create a query and order
+  return order({variety, quantity}, onSuccess, onError);
 }
