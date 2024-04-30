78c94baf321fa3b0cfa3cda340d7919842ea8d08
Nick Korintus <nicholas.korintus@gmail.com>
2024-04-30 16:59:03 -0400

    bash/two-fer solution

--- bash/two-fer/two_fer.sh
+++ bash/two-fer/two_fer.sh
@@ -1,24 +1,11 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+# assign first param to name variable
+name=$1
+
+# use -z flag to return true if string is null
+if [[ -z $name ]]; then
+  echo "One for you, one for me."
+else
+  echo "One for $name, one for me."
+fi
